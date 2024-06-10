5e1edfb552befbd1c1c1e051de901dc2b489d5d8
Mosab Omari <99405259+its-mosab@users.noreply.github.com>
2024-06-11 01:48:02 +0300

    Update and rename telegram-bot.js to index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const TelegramBot = require('node-telegram-bot-api');
 const CryptoJS = require('crypto-js');
 
-const token = '6912363619:AAGuGGvum6dJpyNCOmhpqVuudOeEPpX1Ea8';
-const secretKey = '087GhkpB2IDRcPyNyZ5WUwlr8834fMcBiI7cqN2s1p8K';
+const token = process.env.TOKEN;
+const secretKey = process.env.SECRET_KEY;
 
-// معرفات المستخدمين المسموح لهم باستخدام البوت
 const allowedUsers = [6061488446, 6988698706];
 
 const bot = new TelegramBot(token, { polling: true });
 
-// دالة التشفير
 function encrypt(text) {
   return CryptoJS.AES.encrypt(text, secretKey).toString();
 }
 
-// دالة فك التشفير
 function decrypt(ciphertext) {
   const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
-// الاستماع إلى الرسائل النصية
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const userId = msg.from.id;
   const messageText = msg.text;
 
-  // التحقق من المستخدم
   if (!allowedUsers.includes(userId)) {
     bot.sendMessage(chatId, 'عذراً، أنت غير مصرح لك باستخدام هذا البوت.');
     return;
@@ -34,9 +29,7 @@ bot.on('message', (msg) => {
 
   let response;
 
-  // التحقق إذا كانت الرسالة مشفرة أو نص عادي
   if (messageText.startsWith('ENC:')) {
-    // إذا كانت الرسالة مشفرة، فك تشفيرها
     const encryptedText = messageText.slice(4);
     try {
       response = decrypt(encryptedText);
@@ -44,10 +37,8 @@ bot.on('message', (msg) => {
       response = 'فشل فك التشفير. تأكد من صحة النص.';
     }
   } else {
-    // إذا كانت الرسالة نص عادي، تشفيرها
     response = 'ENC:' + encrypt(messageText);
   }
 
-  // إرسال الرد
   bot.sendMessage(chatId, response);
 });
